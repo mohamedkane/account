e4f52875f0be6edd26d247b87edc156e4a33999a
mohamedkane <43444237+mohamedkane@users.noreply.github.com>
2019-03-26 09:35:58 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,23 @@
+$("button").click(function(){
+     var taka = $(".sun").val() ;
+    console.log(taka);
+        
+        
+        var yen = $(".son").val() ;
+        console.log(yen);
+ 
+ var dollar = $(".sol").val() ;
+        console.log(dollar);
+    
+     
+
+    
+
+
+    $('#nameValue').text(taka);
+   //$("#toll").text("By the year 2080 "+ taka);
+        $('#king').text(dollar);
+        $('#queen').text(yen);
+       
+});
+
